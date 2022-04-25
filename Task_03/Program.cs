968344bf77a14eee08ec034b5f56c741facad47a
RomanGorkavenko <roman@gorkavenko.ru>
2022-04-25 21:56:23 +0300

    add project Task_3

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая будет выдавать
+//название дня недели по заданному номеру.
+
+Console.Write("Введите число от 1 до 7 -> ");
+string dayWeek = Console.ReadLine();
+if (dayWeek == "1") Console.WriteLine("Понедельник");
+else if (dayWeek == "2") Console.WriteLine("Вторник");
+else if (dayWeek == "3") Console.WriteLine("Среда");
+else if (dayWeek == "4") Console.WriteLine("Четверг");
+else if (dayWeek == "5") Console.WriteLine("Пятница");
+else if (dayWeek == "6") Console.WriteLine("Суббота");
+else if (dayWeek == "7") Console.WriteLine("Воскресенье");
+else Console.WriteLine("Ошибка ввода!");
+
