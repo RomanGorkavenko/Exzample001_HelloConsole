6f552d248ccddd5762087d805e25ae851e82f11f
RomanGorkavenko <roman@gorkavenko.ru>
2022-04-25 21:43:12 +0300

    add project Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Введите число 1 -> ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2 -> ");
+int num2 = int.Parse(Console.ReadLine());
+bool result = num1 * num1 == num2;
+Console.WriteLine(result ? "Да" : "Нет");
