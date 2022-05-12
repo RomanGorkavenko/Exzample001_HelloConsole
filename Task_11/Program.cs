0052666335218edc6c39444ee877005e4bb77fd7
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-12 21:43:28 +0300

    new file:   Task_11/Program.cs new file:   Task_12/Program.cs

---
+++ Task_11/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Number = {number}");
+string composNum = СompositeNumber (number);
+Console.WriteLine($"Num {composNum}");
+
+string СompositeNumber (int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    return $"{firstDigit}{thirdDigit}";
+}
---
+++ Task_12/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Число1 ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Число2 ");
+int num2 = int.Parse(Console.ReadLine());
+int num = num1 % num2;
+if (num == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно, остаток {num}");
+}
