6d012b178315da4c7f50f2b5555fc5db2019d2f0
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-12 22:42:24 +0300

    new file:   Task_14/Program.cs new file:   Task_16/Program.cs

---
+++ Task_14/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Проверьте кратно ли число одновременно 7 и 23");
+Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine());
+int first = num % 7;
+int second = num % 23;
+if (first == second)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
---
+++ Task_16/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Число1 ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Число2 ");
+int num2 = int.Parse(Console.ReadLine());
+int num = num1 / num2;
+if (num == num1 || num == num2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
