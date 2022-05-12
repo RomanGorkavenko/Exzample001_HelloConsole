c69f5d0eb1de1c1fbc69c789efcc9fc92cb8dc91
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-12 20:35:37 +0300

    new file:   Task_9/Program.cs

---
+++ Task_9/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Number = {number}");
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+Console.WriteLine($"Number 1 = {firstDigit}");
+Console.WriteLine($"Number 2 = {secondDigit}");
+//if (firstDigit == secondDigit)
+//    Console.WriteLine($"Max = {firstDigit}");
+//else
+//    Console.WriteLine($"Max = {secondDigit}");
+Console.WriteLine(firstDigit > secondDigit ? $"Max = {firstDigit}" : $"Max = {secondDigit}");
