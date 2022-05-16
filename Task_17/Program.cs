b06ce7563e1d6cb4ffb3eb7504782548a0e85bc7
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-16 22:34:56 +0300

    new file:   Task_17/Program.cs new file:   Task_18/Program.cs

---
+++ Task_17/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите координату X: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+int y = int.Parse(Console.ReadLine());
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Диапазон находится в 1 четверти");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("Диапазон находится в 2 четверти");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("Диапазон находится в 3 четверти");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("Диапазон находится в 4 четверти");
+}
+else
+{
+    Console.WriteLine("Значение не должно равняться 0");
+}
---
+++ Task_18/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите номер четверти от 1 до 4: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string range = Number(num);
+Console.WriteLine(range);
+
+string Number(int digit)
+{
+    
+    if (digit == 1) return "x > 0 и y > 0";
+    else if (digit == 2) return "x < 0 и y > 0";
+    else if (digit == 3) return "x < 0 и y < 0";
+    else if (digit == 4) return "x > 0 и y < 0";
+    else return "Некорректный ввод!";
+}
