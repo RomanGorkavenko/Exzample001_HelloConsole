e9152883bbd99d0a36db099095a9016a582466d4
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-18 18:53:43 +0300

    new file:   Example012_1-4_Methods/Program.cs

---
+++ Example012_1_Methods/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Метод. Если метод что-то возвращает, то обязательно указываем тип данных значение которого мы ожидаем
+string Method4(int count, string text) 
+{
+    string result = String.Empty;
+
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+// Вызов метода (аргументы по порядку)
+string res = Method4(10, "z");
+Console.WriteLine(res);
